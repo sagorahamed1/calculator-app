59756ec8ab192b635437899eb39114f7726dc8de
sagor ahamed <sagorahammed002@gmail.com>
2023-10-30 09:16:40 +0600

    first commit scounds commit last commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator_app_sagor/calculator_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Calculator app",
+      home: Calculator(),
+    );
+  }
+}
